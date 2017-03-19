38ab68f5924a0d529b40be2341ba15e8036f6e27
David R. Poindexter III <drpoindexter@gmail.com>
2017-03-19 12:49:14 -0400

    Adding basic app navigation

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Link
+} from 'react-router-dom';
 
 import App from './components/App';
 import About from './components/About';
 import NotFound from './components/NotFound';
 
+const Links = () => (
+    <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+    </nav>
+
+);
+
 const Routes = () => (
     <Router>
         <div>
+            <Links />
             <Switch>
                 <Route exact path="/" component={App}/>
                 <Route path="/about" component={About}/>
